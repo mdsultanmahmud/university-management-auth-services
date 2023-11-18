c796b9dd14ae917d4cc188ebdb476d2f1b0cad12
mdsultanmahmud <mdsultanmahmud.bd00@gmail.com>
2023-11-18 13:01:19 +0600

    unhandled rejection error hanlder done

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,36 @@
 // getting-started.js
 // const mongoose = require('mongoose');
+import { Server } from 'http'
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config'
 import { errorLogger, logger } from './shared/logger'
 async function dbConnect() {
+  let server: Server
   try {
     await mongoose.connect(config.database_url as string)
     logger.info('Database connected')
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       logger.info(`Application is listening on port ${config.port}`)
     })
   } catch (error) {
     errorLogger.error('Failed to connect database')
   }
+
+  // unhandled rejection handling
+  process.on('unhandledRejection', async error => {
+    console.log('unhandleed rejection, we are closing our server...')
+    try {
+      if (server) {
+        await server.close()
+      }
+    } catch (err) {
+      errorLogger.error('Error while closing the server:', err)
+      process.exit(1)
+    }
+    errorLogger.error(error)
+    process.exit(1)
+  })
 }
 
 dbConnect()
